a61c49943be88b6a5b0a20e126eba8a6a97f52b8
Kabir Patel <kabir_patel@hotmail.com>
2018-12-20 17:26:53 +0000

    module 5

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -8,6 +8,13 @@ class QuizQuestion extends Component {
     super(props);
   }
 
+  handleClick(buttonText) {
+    if (buttonText === this.props.quiz_question.answer)
+    {
+      this.props.showNextQuestionHandler();
+    }
+  }
+
   render() {
     return (
       <main>
@@ -16,7 +23,9 @@ class QuizQuestion extends Component {
         </section>
         <section className="buttons">
           <ul>
-              <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
+              {this.props.quiz_question.answer_options.map((answer_option, index) => {
+                return <QuizQuestionButton button_text={answer_option} key={index} clickHandler={this.handleClick.bind(this)} />
+              })}
           </ul>
         </section>
       </main>
